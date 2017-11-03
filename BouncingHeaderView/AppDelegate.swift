c971b9ab51b9ec5678f1fc7907e8ef6199e2565c
Gergely Nemeth <ngergo100@gmail.com>
2017-11-03 15:44:59 +0100

    Initial Commit

---
+++ BouncingHeaderView/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  BouncingHeaderView
+//
+//  Created by Gergely Nemeth on 2017. 11. 03..
+//  Copyright © 2017. Gergely Nemeth. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ BouncingHeaderView/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  BouncingHeaderView
+//
+//  Created by Gergely Nemeth on 2017. 11. 03..
+//  Copyright © 2017. Gergely Nemeth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
